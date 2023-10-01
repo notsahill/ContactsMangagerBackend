bbdd64eef3f79ac25d02f53dca6d4a04751d8a75
notsahill <90394600+notsahill@users.noreply.github.com>
2023-10-01 18:19:24 +0530

    Add files via upload

---
+++ models/contactModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    name:{
+        type: String,
+        required: [true, 'Please provide a name']
+    },
+    email:{
+        type: String,
+        required: [true, 'Please provide a email']
+    },
+    phone:{
+        type: String,
+        required: [true, 'Please provide a phone number']
+    },
+},
+{
+    timestamps: true,    
+}
+);
+
+module.exports = mongoose.model('Contact', contactSchema);
+
